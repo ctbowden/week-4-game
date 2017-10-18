102cb8abb9bd7b6ec28c8cf30eb5b0b15dc70deb
unknown <charles.bowden@gmail.com>
2017-10-18 15:36:57 -0400

    Completed Crystal Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,63 @@
+// Crystal Collector Javascript Game
+// by Charles Bowden
+
+	// Variables
+	var gem1;  // Variable to store value of gem1
+	var gem2;  // Variable to store value of gem2
+	var gem3;  // Variable to store value of gem1
+	var gem4;  // Variable to store value of gem1
+	var matchNumber;  // Variable to store value of Computer's Number to match
+	var userTotal;  // Tracking the user's current score
+	var wins = 0;  // Tracks Player Wins
+	var losses = 0;  // Tracks Player Losses
+
+$(document).ready(function() {
+
+	restart();
+	// // Write the Number to Match to the screen
+	// matchNumber = getRandomInt(19, 120);
+	// $("#computer-pick").text("The number to match is ... " + matchNumber)
+
+	// // Generate Starting Values for Gems
+	// gem1 = getRandomInt(1, 12);
+	// gem2 = getRandomInt(1, 12);
+	// gem3 = getRandomInt(1, 12);
+	// gem4 = getRandomInt(1, 12);
+
+	// // Display Initial Player Score and Wins/Loss
+	// $("#playertotal").text(userTotal);
+	// $("#wins").text(wins);
+	// $("#losses").text(losses);
+
+	// Assign numbers to gems 1-4
+	$("#gem1").on("click", function() {
+		userTotal = gem1 + userTotal;
+		$("#playertotal").text(userTotal);
+		gemClick();
+	});
+
+	$("#gem2").on("click", function() {
+		userTotal = gem2 + userTotal;
+		$("#playertotal").text(userTotal);
+		gemClick();
+	});
+
+	$("#gem3").on("click", function() {
+		userTotal = gem3 + userTotal;
+		$("#playertotal").text(userTotal);
+		gemClick();
+	});
+
+	$("#gem4").on("click", function() {
+		userTotal = gem4 + userTotal;
+		$("#playertotal").text(userTotal);
+		gemClick();
+	});
+
+
+
+
+});
 
 // random number generator for game function
 function getRandomInt(min, max) {
@@ -6,8 +66,42 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-var newrandom;
+// function to check for win condition being met and increase the win/loss counter
+function gemClick() {
+	if (userTotal === matchNumber) {
+		wins++;
+		$("#wins").text(wins);
+		alert("Winner!");
+		restart();
+	} else if (this.userTotal > matchNumber) {
+		losses++;
+		$("#losses").text(losses);
+		alert("Play Again?");
+		restart();
+	} else {
+		// Nothing Happens
+	}
+}
 
-getRandomInt(19, 120);
-newrandom = getRandomInt(19, 120);
-console.log(newrandom);
+function restart() {
+	// Write the Number to Match to the screen
+	matchNumber = getRandomInt(19, 120);
+	$("#computer-pick").text("The number to match is ... " + matchNumber)
+
+	// Generate Starting Values for Gems
+	gem1 = getRandomInt(1, 12);
+	gem2 = getRandomInt(1, 12);
+	gem3 = getRandomInt(1, 12);
+	gem4 = getRandomInt(1, 12);
+	userTotal = 0;
+
+	// Display Initial Player Score and Wins/Loss
+	$("#playertotal").text(userTotal);
+	$("#wins").text(wins);
+	$("#losses").text(losses);
+}
+
+// var newrandom;
+// getRandomInt(19, 120);
+// newrandom = getRandomInt(19, 120);
+// console.log(newrandom);
